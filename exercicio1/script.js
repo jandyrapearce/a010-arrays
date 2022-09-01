a54d64dc9d61f5719991cfd95a3442011cc2a543
jandyra <jandyrapearcebello@gmail.com>
2022-09-01 16:13:23 -0300

    nova alteracao

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const arrayNum = [7, 21, 11, 19]
+const arrayString = ["banana", "chocolate", "morango"]
+const arrayBagunca = ["oi!", 21, true]
+
+console.log(arrayNum)
+console.log(arrayString)
+console.log(arrayBagunca)
